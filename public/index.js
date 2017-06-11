9ec956269c7bc52fadc265c31f33b9e036460a37
SeungRae Kim <kims4@flip1.engr.oregonstate.edu>
2017-06-10 17:16:54 -0700

    Updated index.js(client)

--- public/index.js
+++ public/index.js
@@ -8,17 +8,17 @@ function showCreateSpaceModal(){
 
 function clearCreateSpaceModal(){
   var elem = document.getElementsByClassName('space-input-element');
-  for(var i=0; i<5; i++){
+  for(var i=0; i<2; i++){
     var text = elem[i].querySelector('input');
     text.value='';
   }
-
-  text=elem[3].querySelector('input');
-  text.checked=true;
+  document.getElementById("space-location-input").selectedIndex=0;
   document.getElementById("space-day-input").selectedIndex=0;
-  text=elem[6].querySelector('input');
+  text=elem[3].querySelector('input');
   text.value='';
-  text=elem[7].querySelector('input');
+  text=elem[5].querySelector('input');
+  text.value='';
+  text=elem[6].querySelector('input');
   text.value='';
 }
 
@@ -29,6 +29,46 @@ function hideCreateSpaceModal(){
   back.classList.add('hidden');
   add.classList.add('hidden');
   clearCreateSpaceModal();
+}
+
+function insertNewSpace(){
+
+  var name = document.getElementById('space-name-input').value || '';
+  var address = document.getElementById('space-address-input').value || '';
+  var city = document.getElementById('space-location-input').value || '';
+  var price = document.getElementById('space-price-input').value || '';
+  var picture = document.getElementById('space-picture-input').value || '';
+  var description = document.getElementById('space-description-input').value || '';
+  var day =document.getElementById('space-day-input').value || '';
+
+  if(name.trim()&&address.trim()&&city.trim()&&price.trim()&&picture.trim()&&day.trim()){
+/*    storeNewSpace(name,address,city,price,picture,descrition,function(err){
+      if(err){
+        alert("Unable to save Space. Got this error:\n\n" + err);
+      }  
+      else{
+        var spaceTemplate = Handlebars.templates.space;
+        var templateArgs = {
+          Name: name,
+          Address: address,
+          City: city,
+          Price: price,
+          Day: day,
+          Picture: picture,
+          Description: description 
+        };
+      }
+
+      //Something in here maybe...
+
+       
+    });*/
+  hideCreateSpaceModal();
+  }
+  else{
+    alert("You must specify all values marked by *");
+  }
+
 }
 
 function showLargerViewModal() {
@@ -66,4 +106,16 @@ window.addEventListener('DOMContentLoaded', function(event){
   var closeLargerView = document.getElementById('larger-view-modal').querySelector('.modal-close-button');
   closeLargerView.addEventListener('click',hideLargerViewModal);
 
+  var modalAcceptButton = document.querySelector('.modal-accept-button');
+  if(modalAcceptButton){
+    modalAcceptButton.addEventListener('click',insertNewSpace);
+  }
+
 });
+
+
+
+
+
+
+
